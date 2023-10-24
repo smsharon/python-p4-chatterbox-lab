8de8b31b5a07f86f526fa03c488c3763249a20ae
shallon <shallonmokeira@gmail.com>
2023-10-24 23:01:02 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import asc
 
 from models import db, Message
 
@@ -14,13 +15,97 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(asc(Message.created_at)).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+
+        return response
+
+    elif request.method == 'POST':
+        message = request.get_json()
+        new_message = Message(
+            body=message.get("body"),
+            username=message.get("username")
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response(jsonify(response_body), 404)
+
+        return response
+
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+            return response
+
+        elif request.method == 'PATCH':
+            message = Message.query.filter_by(id=id).first()
+            messagee = request.get_json()
+
+            for attr in messagee:
+              setattr(message,attr,messagee[attr])
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "message deleted."    
+            }
+
+            response = make_response(
+                jsonify(response_body),
+                200
+            )
+
+    return response 
 
 if __name__ == '__main__':
     app.run(port=5555)
